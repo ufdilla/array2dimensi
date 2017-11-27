8aea326f92ec199cafd16f3c63aeacae03cbdf2f
ufdilla <learnmore.cumi@gmail.com>
2017-11-27 10:20:41 +0700

    tambah data

--- src/arraytwodi/tiket.java
+++ src/arraytwodi/tiket.java
@@ -12,7 +12,13 @@ public class tiket
             {"Medan", "Denpasar", "1,300,000"},
             {"Jakarta", "Semarang", "500,000"},
             {"Jakarta", "Denpasar", "700,000"},
-            {"Semarang", "Denpasar", "500,000"}
+            {"Semarang", "Denpasar", "500,000"},
+            {"Semarang", "Jakarta", "400,000"},
+            {"Denpasar", "Jakarta", "600,000"},
+            {"Jakarta", "Medan", "800,000"},
+            {"Semarang", "Medan", "1,000,000"},
+            {"Denpasar", "Medan", "1,200,000"},
+            {"Denpasar", "Semarang", "400,000"}
         };
         
         for (int a = 0; a < data.length; a++)
