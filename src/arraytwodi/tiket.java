e17fea09c5329d3fa22cc041bf793678c12c5f1a
ufdilla <learnmore.cumi@gmail.com>
2017-11-27 10:58:09 +0700

    dijadiin dua data

--- src/arraytwodi/tiket.java
+++ src/arraytwodi/tiket.java
@@ -13,20 +13,39 @@ public class tiket
             {"Jakarta", "Semarang", "500,000"},
             {"Jakarta", "Denpasar", "700,000"},
             {"Semarang", "Denpasar", "500,000"},
-            {"Semarang", "Jakarta", "400,000"},
-            {"Denpasar", "Jakarta", "600,000"},
-            {"Jakarta", "Medan", "800,000"},
-            {"Semarang", "Medan", "1,000,000"},
-            {"Denpasar", "Medan", "1,200,000"},
-            {"Denpasar", "Semarang", "400,000"}
+            
+            {"Jakarta", "Semarang", "400,000"},
+            {"Jakarta", "Denpasar", "600,000"},
+            {"Medan", "Jakarta", "800,000"},
+            {"Medan", "Semarang", "1,000,000"},
+            {"Medan", "Denpasar", "1,200,000"},
+            {"Semarang", "Denpasar", "400,000"}
         };
         
-        for (int a = 0; a < data.length; a++)
+        for (int a = 0; a <= data.length; a++)
         {
+            try 
+            {
+            if(a < (data.length/2))
+            {
             System.out.println("Lokasi Berangkat : " + data[a][0]);
             System.out.println("Lokasi Tujuan : " + data[a][1]);
             System.out.println("Harga Tiket : " + data[a][2]);
             System.out.println("==============================================");
+            }
+            
+            else if(a >= (data.length/2))
+            {
+            System.out.println("Lokasi Berangkat : " + data[a][1]);
+            System.out.println("Lokasi Tujuan : " + data[a][0]);
+            System.out.println("Harga Tiket : " + data[a][2]);
+            System.out.println("==============================================");
+            }
+            }
+            catch (Exception err)
+            {
+                System.out.println( "Error.");
+            }
         }
     }
 }
