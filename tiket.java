bd1d5c14290b0f717779efff5612e802cf8d7ac4
ufdilla <learnmore.cumi@gmail.com>
2017-11-28 11:59:43 +0700

    pake cara agak maksa, repitition tapi tanpa branching selection

--- tiket.java
+++ tiket.java
@@ -3,15 +3,24 @@ import java.util.*;
 public class tiket {
     public static void main(String[] args) {
     
-        int data[][] = 
-        {
-            {0, 800, 1100, 1300}, {800, 0, 500, 700}, {1000, 400, 0, 500}, {1200, 600, 400, 0}
-        };
+        // int data[][] = 
+        // {
+        //     {0, 800, 1100, 1300}, {800, 0, 500, 700}, {1000, 400, 0, 500}, {1200, 600, 400, 0}
+        // };
     
-        String[] dataStr1 = {"Medan", "Jakarta", "Semarang", "Denpasar"};
-        String[][] dataStr2 =
+        // String[] dataStr1 = {"Medan", "Jakarta", "Semarang", "Denpasar"};
+        // String[][] dataStr2 =
+        // {
+        //     {"Medan"},{"Jakarta"}, {"Semarang"}, {"Denpasar"}
+        // };
+
+        String[][] data = 
         {
-            {"Medan"},{"Jakarta"}, {"Semarang"}, {"Denpasar"}
+            { "\t", "Medan", "Jakarta", "Semarang", "Denpasar" },
+            { "Medan", "\t", "800", "1100", "1300" }, 
+            { "Jakarta", "800", "\t", "500", "700" }, 
+            {"Semarang", "1000", "400", "\t", "500" }, 
+            {"Denpasar", "1200", "600", "400", "\t" }
         };
       
         for (int i = 0; i < data.length; i++) { //nested repetition
